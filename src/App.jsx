a287444b02aef8b16a187dea3470b4a109a4a49c
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-05-05 19:43:08 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,34 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [task, newTask] = useState("");
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(task);
+    // envoyer task dans le state taksList
+    // ATTENTION c'est un state tableau
   };
+  const handleTaskChange = (even) => {
+    const value = even.target.value;
+    setTask(value);
+  };
+
   return (
     <>
       <div>
         <h1>To Do List</h1>
         <form onSubmit={handleSubmit}>
-          <input placeholder="task" type="text" name="task" value={newTask}>
-            New Task
-          </input>
+          <input
+            placeholder="Task"
+            type="text"
+            name="task"
+            onChange={handleTaskChange}
+            value={task}
+          />
+          <button>Add task</button>
         </form>
+        {/* faire un .map sur le state tableau taskList */}
       </div>
     </>
   );
