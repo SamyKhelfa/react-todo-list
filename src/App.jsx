5674a6ed5a8b1c5dca5f743305bd7b436910d5e3
Samy Khelfa <samy.khelfa94@gmail.com>
2023-05-07 18:56:24 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,45 @@ function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(task);
-    // envoyer task dans le state taksList
-    // ATTENTION c'est un state tableau
+    setTaskList([...taskList, { text: task, completed: false }]); 
+  setTask("")
   };
-  const handleTaskChange = (even) => {
-    const value = even.target.value;
+  const handleTaskChange = (event) => {
+    const value = event.target.value;
     setTask(value);
   };
+  const handleDelete = (index) => {
+    const newTaskList = taskList.map((task) => task);
+    newTaskList.splice(index, 1)
+    setTaskList(newTaskList)
+  }
 
   return (
-    <>
-      <div>
-        <h1>To Do List</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            placeholder="Task"
-            type="text"
-            name="task"
-            onChange={handleTaskChange}
-            value={task}
-          />
-          <button>Add task</button>
-        </form>
-        {/* faire un .map sur le state tableau taskList */}
-      </div>
-    </>
-  );
-}
+  <>
+    <div>
+      <h1>To Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Task"
+          type="text"
+          name="task"
+          onChange={handleTaskChange}
+          value={task}
+        />
+        <button>Add task</button>
+      </form>
+      <ul>
+        {taskList.map((task, index) => (
+          <li key={index}>
+            <input type="checkbox" />
+            {task.text}
+            <button onClick={() => handleDelete(index)}>🗑️</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </>
+);
+        }
 
 export default App;
